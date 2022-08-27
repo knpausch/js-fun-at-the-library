0c3d15bb5f570964612c0226794f12ae17282157
Keyanna Kiko Pausch <knpausch@gmail.com>
2022-08-27 16:24:23 -0700

    Working on checkoutBook function, need to finish function to pass tests

--- src/Library.js
+++ src/Library.js
@@ -28,13 +28,41 @@ function addBook(library, book)
   {
     library.shelves.nonFiction.push(book);
   }
-
   return library;
+}
 
+function checkoutBook(library, book, genre)
+{
+  var shelfFantasy = library.shelves.fantasy;
+  var shelfFiction = library.shelves.fiction;
+  var shelfNonFiction = library.shelves.nonFiction;
+  var result;
+
+  if (genre === "fantasy")
+  {
+    if(shelfFantasy.includes(book) === true)
+    {
+      return result = `You have now checked out ${book} from the Denver Public Library`;
+    }
+  }
+  if (genre === "fiction")
+  {
+    if(shelfFiction.includes(book) === true)
+    {
+      return result = `You have now checked out ${book} from the Denver Public Library`;
+    }
+  }
+  if (genre === "nonFiction")
+  {
+    if(shelfNonFiction.includes(book) === true)
+    {
+      return result = `You have now checked out ${book} from the Denver Public Library`
+    }
+  }
 }
 
 module.exports = {
   createLibrary,
   addBook,
-  // checkoutBook
+  checkoutBook
 };
